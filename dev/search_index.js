e761147a4baab83042fca90a4fcb2528215eeb89
Documenter.jl <documenter@juliadocs.github.io>
2025-04-17 20:46:45 +0000

    build based on d6930cf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TCMGreensFunctions","category":"page"},{"location":"#TCMGreensFunctions","page":"Home","title":"TCMGreensFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TCMGreensFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TCMGreensFunctions]","category":"page"}]
+}
